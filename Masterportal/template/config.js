1bfd3574206301fa3fc41c681b543442bfbc2723
Lisa-Stubert <61182572+Lisa-Stubert@users.noreply.github.com>
2021-09-27 17:55:11 +0200

    workshop test

---
+++ Masterportal/template/config.js
@@ -0,0 +1,38 @@
+const Config = {
+    wfsImgPath: "./resources/img/",
+    metadata: {
+        useProxy: [
+            "https://metaver.de/csw"
+        ]
+    },
+    namedProjections: [
+        ["EPSG:25833", "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
+    ],
+    footer: {
+        urls: [
+            {
+                "bezeichnung": "common:modules.footer.designation",
+                "url": "https://www.stadtentwicklung.berlin.de", // hier URL eintragen
+                "alias": "Bezirksamt XY", // hier Namen des Bezirksamts eintragen
+                "alias_mobil": "BA XY"
+            }
+        ],
+        showVersion: true
+    },
+    quickHelp: {
+        imgPath: "./resources/img/"
+    },
+    layerConf: "./resources/services-internet.json",
+    restConf: "./resources/rest-services-internet.json",
+    styleConf: "./resources/style_v3.json",
+    scaleLine: true,
+    mouseHover: {
+        numFeaturesToShow: 2,
+        infoText: "(weitere Objekte. Bitte zoomen.)"
+    }
+};
+
+// conditional export to make config readable by e2e tests
+if (typeof module !== "undefined") {
+    module.exports = Config;
+}
